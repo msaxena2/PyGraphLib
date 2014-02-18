7776c16cc505544562b99623d8c74f7183cc0ae2
Manasvi Saxena <msaxena2@illinois.edu>
2014-02-18 12:36:49 -0600

    added docs

--- PyGraphLib.py
+++ PyGraphLib.py
@@ -1,11 +1,11 @@
-'''
+"""
 Created on Sep 26, 2013
 
 Graph library to create
 directional/undirectional graphs with weighted/un-weighted edges
 
-@author: Manasvi Saxena
-'''
+:author: Manasvi Saxena
+"""
 from operator import itemgetter
 from Queue import PriorityQueue
 import sys
@@ -29,26 +29,26 @@ class graph:
         
         
     def add_new_node(self, node_key, node_data):
-        '''
+        """
         Adds a new node to the dictionary
         
-        @param node_key: node_key: key of new node
-        @param node_data: data associated with node
-        @return: status of the operation
-        '''
+        :param node_key: node_key: key of new node
+        :param node_data: data associated with node
+        :return: status of the operation
+        """
         self.data_dict[node_key] = node_data
         self.graph_connections_dict[node_key] = dict()
         return OPERATION_SUCCESSFUL
         
     def add_connection(self, origin_node_key,
                        destination_node_key, edge_weight):
-        '''
+        """
         Adds connection between the specified nodes
-        @param origin_node_key: origin_node_key: key of origin node
-        @param destination_node_key: destination_node_key: key of destination node
-        @return: status of operation
-        
-        ''' 
+
+        :param origin_node_key: origin_node_key: key of origin node
+        :param destination_node_key: destination_node_key: key of destination node
+        :return: status of operation
+        """ 
         if origin_node_key not in self.data_dict \
             or destination_node_key not in self.data_dict:
             return NODE_NOT_PRESENT_ERROR;
@@ -58,23 +58,25 @@ class graph:
             
     
     def get_node_data(self, node_key):
-        '''
+        """
         Gets the data associated with the node
-        @param node_key: key of the node
-        @return: data of the node or error message if node not found
-        '''
+
+        :param node_key: key of the node
+        :return: data of the node or error message if node not found
+        """
         if node_key not in self.data_dict:
             return KEY_NOT_FOUND_ERROR
         return self.data_dict[node_key]    
     
     
     def is_direct_path_present(self, origin_node, destinaiton_node):
-        '''
+        """
         check whether direct path exists between two nodes
-        @param origin_node: key of the origin node
-        @param destination_node: Key of the destination node
-        @return: Success or error codes
-        ''' 
+
+        :param origin_node: key of the origin node
+        :param destination_node: Key of the destination node
+        :return: Success or error codes
+        """ 
         if origin_node not in self.data_dict \
             or destinaiton_node not in self.data_dict:
             return NODE_NOT_PRESENT_ERROR
@@ -85,11 +87,12 @@ class graph:
     
    
     def get_path_weight(self, origin_node, destination_node):
-        '''
+        """
         gets the weight of path associated with specified origin node
-        @param origin_node: Key of the origin node
-        @return: Weight or error code
-        '''
+
+        :param origin_node: Key of the origin node
+        :return: Weight or error code
+        """
         if origin_node not in self.data_dict \
             or destination_node not in self.data_dict:
             return NODE_NOT_PRESENT_ERROR
@@ -99,20 +102,21 @@ class graph:
         return PATH_NOT_FOUND
     
     def get_all_data(self):
-        '''
+        """
         return all data in the graph in the form of a list
-        '''
+        """
         return_val = list()
         for key in self.data_dict:
             return_val.append(self.data_dict[key])
         return return_val
     
     def get_all_neighbors(self, node_key):
-        '''
+        """
         return all the neighbor nodes' keys
-        @param node_key: key of origin node
-        @return: List of all neighbors' keys and weights
-        '''
+
+        :param node_key: key of origin node
+        :return: List of all neighbors' keys and weights
+        """
         if node_key not in self.graph_connections_dict:
             return KEY_NOT_FOUND_ERROR
         return_val = self.graph_connections_dict[node_key]
@@ -125,9 +129,9 @@ class graph:
         return return_list
     
     def get_sorted_data_list(self):
-        '''
+        """
         returns all data in list sorted by key
-        '''
+        """
         return_list = list();
         for node_key in self.graph_connections_dict:
             temp_dict = self.graph_connections_dict[node_key]
@@ -139,23 +143,23 @@ class graph:
         return sorted_return_list      
     
     def get_cheapest_edge(self):
-        '''
+        """
         return the cheapest edge i.e. edge with minimum weight
-        '''
+        """
         data_list = self.get_sorted_data_list()
         return data_list[0];
    
     def get_costliest_edge(self):
-        '''
+        """
         returns costliest edge i.e. edge with most weight
-        '''
+        """
         data_list = self.get_sorted_data_list()
         return data_list[-1];
     
     def get_average_edge_cost(self):
-        ''' 
+        """ 
         returns the average weight of all edges
-        '''
+        """
         data_list = self.get_sorted_data_list();
         edge_sum = 0 
         edge_count = 0
@@ -165,9 +169,9 @@ class graph:
         return edge_sum / edge_count
     
     def get_nodelist_by_degree(self):
-        '''
+        """
         return a list of nodes sorted by degrees
-        '''
+        """
         degree_dict = dict()
         for node_key in self.graph_connections_dict:
             degree = len(self.graph_connections_dict[node_key])
@@ -177,13 +181,13 @@ class graph:
         
         
     def remove_node(self, node_key):
-        '''
+        """
         Removes the specified node from the graph. Also removes the connections
         associated with the node
         
-        @param node_key: the key of the node to be removed
-        @return : Status message of the operation
-        '''
+        :param node_key: the key of the node to be removed
+        :return : Status message of the operation
+        """
         if node_key not in self.data_dict or node_key not in self.graph_connections_dict:
             return KEY_NOT_FOUND_ERROR
         del self.graph_connections_dict[node_key]
@@ -197,16 +201,16 @@ class graph:
         return OPERATION_SUCCESSFUL
     
     def remove_connection(self, origin_node_key, destination_node_key):
-        '''
+        """
         Removes the connection between the specified origin node and the specified destination node
         Keep in mind that this only removes the connection in one direction, for undirected graphs, 
         the function must be called again with the destination node as the origin node and origin node as the
         destination node
         
-        @param origin_node_key: the key of the origin node
-        @param destination_node_key: the key of the destination node
-        @return: status of the operation
-        '''
+        :param origin_node_key: the key of the origin node
+        :param destination_node_key: the key of the destination node
+        :return: status of the operation
+        """
         if origin_node_key not in self.data_dict or origin_node_key not in self.graph_connections_dict:
             return KEY_NOT_FOUND_ERROR
         if destination_node_key not in self.data_dict or destination_node_key not in self.graph_connections_dict:
@@ -217,25 +221,26 @@ class graph:
         return OPERATION_SUCCESSFUL
                    
     def get_degree(self, node_key):
-        '''
+        """
         Returns The number degree of specified node
-        @param node_key: The key of the node for finding the degree
-        @return: The degree or error code
-        '''
+        
+        :param node_key: The key of the node for finding the degree
+        :return: The degree or error code
+        """
         if node_key not in self.graph_connections_dict \
         or node_key not in self.data_dict:
             return KEY_NOT_FOUND_ERROR
         return len(self.graph_connections_dict[node_key]) 
     
     def get_shortest_path(self, origin_key, destination_key):
-        '''
+        """
         Uses Djikstra's shortest path algorithm to return the shortest path
         between the specified nodes
         
-        @param origin_key: The key of the origin node
-        @param destinaiton_key: The key of the destination node
-        @return: The path in the form of an array or path not found error
-        '''
+        :param origin_key: The key of the origin node
+        :param destinaiton_key: The key of the destination node
+        :return: The path in the form of an array or path not found error
+        """
         
         path_dict = dict()
         unvisited_list = list()
@@ -279,9 +284,3 @@ class graph:
                 path_list.reverse()
                 return path_list
     
-
-    
-
-        
-            
-            
